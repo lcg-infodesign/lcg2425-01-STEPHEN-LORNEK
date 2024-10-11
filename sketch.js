a6a877530e6895348572af87c5156800535f1d6d
Lorenzo Coppi <lorenzomaria.coppi@icloud.com>
2024-10-11 12:54:39 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,34 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  createCanvas(windowWidth, windowHeight); // schermo che si adatta
+  background(255); // Imposta lo sfondo bianco
+
+  let r = 10; 
+  let diameter = r * 2; // raggio e diametro dei cerchi
+  let rows = floor(height / diameter); // righe
+  let cols = floor(width / diameter); // colonne
+  let centerRow = floor(rows / 2);
+  let centerCol = floor(cols / 2);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let x = j * diameter + r; 
+      let y = i * diameter + r; // Posizione x e y al centro 
+
+      // cerchi verticali e orizzontali
+      let distanceFromVerticalCenter = abs(j - centerCol);
+      let distanceFromHorizontalCenter = abs(i - centerRow);
+      let minDistance = min(distanceFromVerticalCenter, distanceFromHorizontalCenter);
+
+      // Spessore del cerchio che diminuisce gradualmente con la distanza dalla linea centrale
+      let maxThickness = 15; // Spessore massimo 
+      let minThickness = 1;  // Spessore minimo
+      let thicknessVariation = map(minDistance, 0, max(centerRow, centerCol), maxThickness, minThickness);
+      let randomThickness = random(thicknessVariation / 2, thicknessVariation); // metto casuale lo spessore in base alla distanza
+
+      strokeWeight(randomThickness); // 
+      noFill(); //nessun riempimento
+      ellipse(x, y, diameter, diameter); // cerchio
+    }
+  }
 }
 
-function draw() {
-  // put drawing code here
-}
